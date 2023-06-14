e889c20a2e98dfc2d9a006f4c6893efebd4d7d3c
DESKTOP-KS71IIP\Justin <jken.alvarez@gmail.com>
2023-06-14 11:12:35 -0400

    Added New Files

---
+++ shared/src/commonMain/resources/theme/Shape.kt
@@ -0,0 +1,15 @@
+package ui.theme
+
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+
+val ThemeShapes = Shapes(
+    //small = CutCornerShape(40.dp),
+    small = RoundedCornerShape(5.dp),
+    medium = RoundedCornerShape(20.dp),
+    large = RoundedCornerShape(0.dp)
+)
