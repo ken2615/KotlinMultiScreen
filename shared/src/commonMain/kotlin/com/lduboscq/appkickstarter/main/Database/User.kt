95f3aa3a543b1f8b6cb9c0a7a1c328bcd78e2bb6
DESKTOP-KS71IIP\Justin <jken.alvarez@gmail.com>
2023-06-15 13:12:09 -0400

    Added New Files (My own database)

---
+++ shared/src/commonMain/kotlin/com/lduboscq/appkickstarter/main/Database/User.kt
@@ -0,0 +1,22 @@
+package com.lduboscq.appkickstarter.main.Database
+
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.RealmUUID
+
+class User: RealmObject {
+
+    var _id: String = RealmUUID.random().toString()
+    var name: String = ""
+    var password: String = ""
+    var email: String = ""
+    var bmi: Int = 0
+}
+
+data class UserData(
+    var id: String? = null,
+    var name: String = "",
+    var password: String = "",
+    var email: String = "",
+    var bmi: Int = 0,
+    var user: User?
+)
---
+++ shared/src/commonMain/kotlin/com/lduboscq/appkickstarter/main/Database/UserRepository.kt
@@ -0,0 +1,64 @@
+package com.lduboscq.appkickstarter.main.Database
+
+import io.realm.kotlin.Realm
+import io.realm.kotlin.types.RealmUUID
+
+abstract class UserRepository {
+    lateinit var realm: Realm
+
+    abstract suspend fun setupRealmSync()
+
+    suspend fun getUserData(user: User?) : UserData? {
+        if(!this::realm.isInitialized) {
+            setupRealmSync()
+        }
+        var userData : UserData? = null
+        realm.write {
+            if(user != null){
+                userData = UserData(
+                    id = user!!._id,
+                    name = user!!.name,
+                    password = user!!.password,
+                    email = user!!.email,
+                    bmi = user!!.bmi,
+                    user = user
+                )
+            }
+        }
+        return userData
+    }
+
+    private fun closeRealmSync() {
+        realm.close()
+    }
+
+    /** Adds user
+     * */
+    suspend fun addUser(userData: UserData) : UserData? {
+        if(!this::realm.isInitialized) {
+            setupRealmSync()
+        }
+
+        var user2: User? = null
+        realm.write {
+            user2 = this.copyToRealm(User().apply {
+                _id = userData.id ?: RealmUUID.random().toString()
+                name = userData.name
+                password = userData.password
+                email = userData.email
+                bmi = userData.bmi
+            })
+        }
+        return getUserData(user2)
+    }
+
+    // returns the first user that matches the given name
+    suspend fun getUser(userName : String): UserData? {
+        if(!this::realm.isInitialized) {
+            setupRealmSync()
+        }
+        val user : User? = realm.query<User>(User::class, "name = \"$userName\"").first().find()
+        return getUserData(user)
+
+    }
+}
